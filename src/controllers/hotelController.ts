98df0f33075b3bd0d85247ed143d2ce7898a20d7
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-11-07 00:15:42 +0530

    create hotel controller added

--- src/controllers/hotelController.ts
+++ src/controllers/hotelController.ts
@@ -5,3 +5,17 @@ export const getHotels = async (req: Request, res: Response) => {
   const hotels = await HotelModel.find({});
   return res.status(200).json(hotels);
 };
+
+export const createHotel = async (req: Request, res: Response) => {
+  const { name, city, state, country, zipCode, noOfRooms } = req.body;
+  if (!name || !city || !state || !country || !zipCode || !noOfRooms)
+    return res.status(400).json({
+      msg: "Please provide name, city, state, country, zipCode & noOfRooms",
+    });
+
+  const hotel = await HotelModel.find({ where: { name } });
+  if (hotel) return res.status(400).json({ msg: "Hotel with provided name already exists" });
+
+  const newHotel = await HotelModel.create({ name, city, state, country, zipCode, noOfRooms });
+  return res.status(201).json(newHotel);
+};
--- src/routes/hotelRoutes.ts
+++ src/routes/hotelRoutes.ts
@@ -1,8 +1,8 @@
-import { getHotels } from "../controllers/hotelController";
+import { createHotel, getHotels } from "../controllers/hotelController";
 import express from "express";
 
 const router = express.Router();
 
-router.route("/").get(getHotels);
+router.route("/").get(getHotels).post(createHotel);
 
 export default router;
